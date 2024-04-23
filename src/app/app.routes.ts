b0829706c2cfff14ff554998df039a3d9bd03056
Vanina Quezada <vaninaquezada@gmail.com>
2024-04-23 19:16:54 -0300

    sprint 1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuiensoyComponent } from './components/quiensoy/quiensoy.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'quiensoy', component: QuiensoyComponent },
+  { path: '**', component: HomeComponent },
+];
---
+++ src/app/components/quiensoy/quiensoy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiensoy',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiensoy.component.html',
+  styleUrl: './quiensoy.component.css'
+})
+export class QuiensoyComponent {
+
+}
